8a67bd399a01d44b2c5208bf64a10a49cf7df413
ourdarkchemistry <ourdarkchemistry@gmail.com>
2024-10-12 14:15:20 +0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,20 +5,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\ReportController;
 
-// User API routes
 Route::prefix('users')->group(function () {
-    Route::get('/', [UserController::class, 'index']); // Get all users
-    Route::get('{id}', [UserController::class, 'show']); // Get a specific user
-    Route::post('/', [UserController::class, 'store']); // Create a new user
-    Route::put('{id}', [UserController::class, 'update']); // Update a specific user
-    Route::delete('{id}', [UserController::class, 'destroy']); // Delete a specific user
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('{id}', [UserController::class, 'show']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::put('{id}', [UserController::class, 'update']);
+    Route::delete('{id}', [UserController::class, 'destroy']);
 });
 
-// Report API routes
 Route::prefix('reports')->group(function () {
-    Route::get('/', [ReportController::class, 'index']); // Get all reports
-    Route::get('{id}', [ReportController::class, 'show']); // Get a specific report
-    Route::post('/', [ReportController::class, 'store']); // Create a new report
-    Route::put('{id}', [ReportController::class, 'update']); // Update a specific report
-    Route::delete('{id}', [ReportController::class, 'destroy']); // Delete a specific report
+    Route::get('/', [ReportController::class, 'index']);
+    Route::get('{id}', [ReportController::class, 'show']);
+    Route::post('/', [ReportController::class, 'store']);
+    Route::put('{id}', [ReportController::class, 'update']);
+    Route::delete('{id}', [ReportController::class, 'destroy']);
 });
